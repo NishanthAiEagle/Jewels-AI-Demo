d151bfd16b42c44937b5a7f393554d21f422dbb7
NishanthAiEagle <nishanthoverlay@gmail.com>
2025-12-02 08:36:41 +0000

    F4

--- script.js
+++ script.js
@@ -322,7 +322,7 @@ function drawJewelry(state, ctx) {
 
   /* Earrings */
   if (earringImg) {
-    const ew = earDist * 0.18;
+    const ew = earDist * 0.24;
     const eh = (earringImg.height / earringImg.width) * ew;
 
     ctx.drawImage(earringImg, leftEar.x - ew/2,  leftEar.y - eh/2,  ew, eh);
